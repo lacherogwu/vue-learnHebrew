d6b2b143425bd9137b9ee683a4bc0ab5340811e9
lachero <asaf@utopia.tech>
2021-10-17 21:14:44 +0300

    removed old swipe package

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ import App from './App.vue';
 import './index.css';
 import './firebase/init.js';
 import globalComponents from './globalComponents';
-import Vue3TouchEvents from 'vue3-touch-events';
 
-const app = createApp(App).use(router).use(store).use(Vue3TouchEvents);
+const app = createApp(App).use(router).use(store);
 
 // load global components
 globalComponents.forEach(component => app.component(component.name, component));
