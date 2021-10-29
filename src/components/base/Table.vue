7f9c95d06403a835e97d13d06222aeb7c210da45
lachero <asaf@utopia.tech>
2021-10-29 23:10:06 +0300

    fixed mobile long click

--- src/components/base/Table.vue
+++ src/components/base/Table.vue
@@ -13,7 +13,18 @@
 							</tr>
 						</thead>
 						<tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-700 dark:divide-gray-600">
-							<tr v-for="item in items" :key="items.id" class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600" @click="$router.push(`/words/update/${item.id}`)" @mousedown.prevent="mouseDown(item)" @mouseup="clear" @mouseleave="clear">
+							<tr
+								v-for="item in items"
+								:key="items.id"
+								class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
+								@click="$router.push(`/words/update/${item.id}`)"
+								@mousedown.prevent="removeItem(item)"
+								@mouseup="clear"
+								@mouseleave="clear"
+								@touchstart.prevent="removeItem(item)"
+								@touchend="clear"
+								@touchcancel="clear"
+							>
 								<td class="px-6 md:py-4 whitespace-nowrap">
 									<div class="text-sm text-gray-900 dark:text-gray-100">{{ item.hebrewTranslation }}</div>
 									<div class="text-sm text-gray-500 dark:text-gray-400">{{ item.russianTranslation }}</div>
@@ -71,7 +82,7 @@ const timeout = ref();
 const clear = () => {
 	clearTimeout(timeout.value);
 };
-const mouseDown = item => {
+const removeItem = item => {
 	timeout.value = setTimeout(() => {
 		emit('action', 'remove', { id: item.id });
 	}, 500);
