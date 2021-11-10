cf2bc27422a07f084b5365d2fdde9227367b0826
lachero <asaf@utopia.tech>
2021-11-10 21:39:55 +0200

    support PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [
+		vue(),
+		VitePWA({
+			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'Hebrew Cards',
+				short_name: 'Hebrew Cards',
+				description: 'Memorize Hebrew using cards',
+				theme_color: '#1f2937',
+				icons: [
+					{
+						src: 'android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
+});
